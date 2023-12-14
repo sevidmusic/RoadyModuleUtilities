1dac7b53b901c13beac505896847b2bb6198ae7d
sevidmusic <sevidmusic@gmail.com>
2023-12-13 19:13:56 -0500

    Defined new test method in `tests/RoadyModuleUtilitiesTest.php`:
    ```
    RoadyModuleUtilitiesTest->pathToRoadyTestModuleDirectory()
    ```

--- tests/RoadyModuleUtilitiesTest.php
+++ tests/RoadyModuleUtilitiesTest.php
@@ -2,6 +2,7 @@
 
 namespace Darling\RoadyModuleUtilities\tests;
 
+use \Darling\PHPFileSystemPaths\classes\paths\PathToExistingDirectory;
 use \Darling\PHPTextTypes\classes\collections\SafeTextCollection;
 use \Darling\PHPTextTypes\classes\strings\Name;
 use \Darling\PHPTextTypes\classes\strings\SafeText;
@@ -9,7 +10,10 @@ use \Darling\PHPTextTypes\classes\strings\Text;
 use \Darling\PHPUnitTestUtilities\traits\PHPUnitConfigurationTests;
 use \Darling\PHPUnitTestUtilities\traits\PHPUnitRandomValues;
 use \Darling\PHPUnitTestUtilities\traits\PHPUnitTestMessages;
+use \Darling\RoadyModuleUtilities\classes\paths\PathToDirectoryOfRoadyModules;
+use \Darling\RoadyModuleUtilities\classes\paths\PathToRoadyModuleDirectory;
 use \PHPUnit\Framework\TestCase;
+use \DirectoryIterator;
 
 /**
  * Defines common methods that may be useful to all
@@ -67,4 +71,44 @@ class RoadyModuleUtilitiesTest extends TestCase
         );
     }
 
+    public function pathToRoadyTestModuleDirectory(): PathToRoadyModuleDirectory
+    {
+        $partsOfPathToRoadyModuleUtilitiesTestsDirectory =
+            $this->safeTextCollectionThatMapsToTheRoadyModuleUtilitiesLibrarysTestsDirectory();
+        $partsOfPathToRoadyModuleUtilitiesTestModulesDirectory = [];
+        foreach(
+            $partsOfPathToRoadyModuleUtilitiesTestsDirectory->collection()
+            as
+            $part
+        )
+        {
+            $partsOfPathToRoadyModuleUtilitiesTestModulesDirectory[] =
+                $part;
+        }
+        $partsOfPathToRoadyModuleUtilitiesTestModulesDirectory[] =
+            new SafeText(new Text('modules'));
+        $safeTextCollectionOfPartsOfPathToRoadyModuleUtilitiesTestModulesDirectory =
+            new SafeTextCollection(
+                ...$partsOfPathToRoadyModuleUtilitiesTestModulesDirectory
+            );
+        $pathToRoadyModuleUtilitiesTestModulesDirectory =
+            new PathToExistingDirectory(
+                $safeTextCollectionOfPartsOfPathToRoadyModuleUtilitiesTestModulesDirectory
+            );
+        $testModuleDirectory = new DirectoryIterator($pathToRoadyModuleUtilitiesTestModulesDirectory->__toString());
+        $testModuleNames = [];
+        foreach($testModuleDirectory as $fileInfo) {
+            if($fileInfo->isDir() && !$fileInfo->isDot()) {
+                $testModuleNames[] = $fileInfo->getFilename();
+            }
+        }
+        $targetModuleName = $testModuleNames[array_rand($testModuleNames)];
+        $pathToDirectoryOfRoadyTestModules = new PathToDirectoryOfRoadyModules(
+            $pathToRoadyModuleUtilitiesTestModulesDirectory,
+        );
+        return new PathToRoadyModuleDirectory(
+            $pathToDirectoryOfRoadyTestModules,
+            new Name(new Text($targetModuleName)),
+        );
+    }
 }
